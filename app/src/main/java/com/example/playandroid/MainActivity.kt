42abf5c555b0f34e7b32f3e01142ddb43d7883bf
bryanrady <bryanrady@163.com>
2020-10-17 14:46:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/playandroid/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.playandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://www.wanandroid.com/index
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
